2786a92384268eea7ed19e9248453a263b45e49f
Hidayatullah Wildan Ghaly Buchary <16521502@std.stei.itb.ac.id>
2023-03-12 21:33:43 +0700

    fix: map and vertex

--- Map.cs
+++ Map.cs
@@ -8,67 +8,119 @@ public class Map
 	/* Gerakan yang mungkin
 	 * Up, Right, Down, Left */
 	int[] MoveX = { 0, 1, 0, -1 };
-	int[] MoveY = { 1, 0, -1, 0 };
+	int[] MoveY = { -1, 0, 1, 0 };
 
 	/* Ini buat DFS BFS */
-	Vertex[] Buffer = new Vertex[MapX*MapY];
-	char[,] map = new char[MapY,MapX];
+	Vertex[] Buffer;
+	char[,] map;
 
 	/* MAP Matrix of what */
 	/* x dan y posisi, map peta, */
 
 	/* Validator */
-	bool isUpValid(Vertex point, char[][] map)
+	public bool isUpValid(Vertex point, char[,] map)
 	{
-		if(map[point.y+MoveY[0],point.x+MoveX[0]] != 'X' && point.x + MoveX[0] <= MapX && point.y + MoveY[0] <= MapY){ 
-			return true; 
-		} else { 
-			return false; 
+		if (point.x + MoveX[0] <= MapX && point.y + MoveY[0] <= MapY && point.x + MoveX[0] >= 0 && point.y + MoveY[0] >= 0){
+			return (map[point.y-MoveY[0],point.x+MoveX[0]] != 'X');
+		} else {
+			return false;
 		}
 	}
 
-	bool isDownValid(Vertex point, char[][] map)
+	public bool isDownValid(Vertex point, char[,] map)
 	{
-		if(map[point.y+MoveY[2],point.x+MoveX[2]] != 'X' && point.x + MoveX[0] <= MapX && point.y + MoveY[0] <= MapY ){ return true; } else { return false; }
-	}
-	bool isRightValid(Vertex point, char[][] map)
+		if (point.x + MoveX[2] <= MapX && point.y + MoveY[2] <= MapY && point.x + MoveX[2] >= 0 && point.y + MoveY[2] >= 0){
+			return (map[point.y + MoveY[2],point.x + MoveX[2]] != 'X');
+		} else {
+			return false;
+		}
+	}	
+	public bool isRightValid(Vertex point, char[,] map)
 	{
-		if(map[point.y+MoveY[1],point.x+MoveX[1]] != 'X' && point.x + MoveX[0] <= MapX && point.y + MoveY[0] <= MapY ){ return true; } else { return false; }
-	}
-	bool isLeftValid(Vertex point, char[][] map)
+		if (point.x + MoveX[1] <= MapX && point.y + MoveY[1] <= MapY && point.x + MoveX[1] >= 0 && point.y + MoveY[1] >= 0){
+			return (map[point.y + MoveY[1],point.x + MoveX[1]] != 'X');
+		} else {
+			return false;
+		}	}
+	public bool isLeftValid(Vertex point, char[,] map)
 	{
-		if(map[point.y+MoveY[0],point.x+MoveX[0]] != 'X' && point.x + MoveX[0] <= MapX && point.y + MoveY[0] <= MapY ){ return true; } else { return false; }
+		if (point.x + MoveX[3] <= MapX && point.y + MoveY[3] <= MapY && point.x + MoveX[3] >= 0 && point.y + MoveY[3] >= 0){
+			return (map[point.y + MoveY[3],point.x + MoveX[3]] != 'X');
+		} else {
+			return false;
+		}
 	}
 
 	public Map()
 	{
 		MapX = 0;
 		MapY = 0;
+		map = new char[MapY, MapX];
+		Buffer = new Vertex[MapX * MapY];
 	}
-
-	/* Baca Peta dari sebuah file */
+	
 	public Map(string file)
 	{
-		/* Hitung baris kolom */
 		string[] lines = System.IO.File.ReadAllLines(file);
-		int county = 0;
-		int countx = 1;
+		int countY = 0;
+		int countX = 1;
 		foreach(string line in lines){
-			county++;
+			countY++;
 
-			if(county == 1){
+			if(countY == 1){
 				foreach(char word in line){
 					if(word == ' '){
-						countx++;
+						countX++;
 					}
 				}
 			}
 
 		}
-		MapX = countx;
-		MapY = county;
-		
-		/* Masukin ke Matrixnya */
+		MapX = countX;
+		MapY = countY;
 
+		map = new char[countY, countX];
+		for (int y = 0; y < countY; y++)
+		{
+			string line = lines[y];
+			int j = 0;
+			for (int x = 0; x < line.Length; x+=2)
+			{
+				char c = line[x];
+				map[y, j] = c;
+				j++;
+			}
+		}
+
+		Buffer = new Vertex[countY * countX];
+		Console.Write(countX + " " + countY + "\n");
+		for (int i = 0; i < countY; i++){
+			for (int j = 0; j < countX; j++){
+				if (map[i,j] == 'X'){
+					Buffer[i * countY + j] = new Vertex(i, j, false, false);
+				} else if (map[i,j] == 'T'){
+					Buffer[i * countY + j] = new Vertex(i, j, true, true);
+				} else { /* K atau R */
+					Buffer[i * countY + j] = new Vertex(i, j, false, true);
+				}
+			}
+		}
+
+
+
+		// Print the matrix for testing purposes
+		for (int y = 0; y < countY; y++)
+		{
+			for (int x = 0; x < countX; x++)
+			{
+				Console.Write(map[y, x] + " ");
+			}
+			Console.WriteLine();
+		}
+	}
+
+	public char[,] getMap()
+	{
+		return map;
 	}
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Map m = new Map("config.txt");
+Vertex p = new Vertex(3, 3, false, false);
+if (m.isDownValid(p, m.getMap())){
+    Console.Write("Down is valid\n");
+}
+
+if (m.isRightValid(p, m.getMap())){
+    Console.Write("Right is valid\n");
+}
+
+if (m.isLeftValid(p, m.getMap())){
+    Console.Write("Left is valid\n");
+}
+
+if (m.isUpValid(p, m.getMap())){
+    Console.Write("Up is valid\n");
+}

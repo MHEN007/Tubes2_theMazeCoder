3244fd6de3bcce38b345d56090c0fedf0e19599a
MHEN2606 <matthew.mahendra@gmail.com>
2023-03-18 16:43:13 +0700

    Semoga bener bs system.windows.forms

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // See https://aka.ms/new-console-template for more information
+using System;
+using System.Windows.Forms;
 Console.WriteLine("Hello, World!");
 
 Solver solver = new Solver();
